150d3af1681682381c9cc304bcedfb763132d193
nprasannavenkatesh <tsoprav@gmail.com>
2020-06-21 09:45:06 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,19 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+/*color1.addEventListener("input",function(){
+	console.log(color1.value);
+})
+
+color2.addEventListener("input",function(){
+	console.log(color2.value);
+}) */
+
+function SetGradient() {
+	body.style.background = "linear-gradient(to right,"+ color1.value + "," + color2.value + ")";
+}
+
+color1.addEventListener("input",SetGradient);
+
+color2.addEventListener("input",SetGradient);
